3f300c23ab78c08a5cf8c51889b39707cab3beb7
euancz <162553153+euancz@users.noreply.github.com>
2026-08-15 02:14:04 +0000

    Finished Ui for the converter

---
+++ main.py
@@ -0,0 +1,32 @@
+choices = {1: "Farenheit to Celcius", 
+           2: "Celcius To Farenheit", 
+        3: "Celcius To Kelvin"}
+print(f"=" * 50)
+
+print(f"This is a Temperature Converter pick from the following: ")
+print(f"=" * 50)
+
+for key, value in choices.items():
+    print(f"{key} = {value}")
+print(f"=" * 50)
+choice = int(input("Enter choice: "))
+print(f"=" * 50)
+
+if(choice == 1):
+    Farenheit = int(input("Enter Farenheit"))
+    Celcius = int(input("Enter Celcius"))
+    print(f"=" * 50)
+    
+elif (choice == 2):
+    Celcius = int(input("Enter Celcius"))
+    Farenheit = int(input("Enter Farenheit"))
+    print(f"=" * 50)
+    
+elif (choice == 3):
+    Celcius = int(input("Enter Celcius"))
+    Kelvin = int(input("Enter Kelvin"))
+    print(f"=" * 50)
+
+else:
+    print("Invalid Choice")
+    print(f"=" * 50)
