b8b6b0e4b481d530399912e043e192b9ccea2cab
euancz <162553153+euancz@users.noreply.github.com>
2026-08-17 00:34:20 +0800

    Fixed issues with merge
    fixed issues with the merge and fixed the accidentally deleted lines of codes

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 choices = {1: "Farenheit to Celcius", 
            2: "Celcius To Farenheit", 
-        3: "Celcius To t(f"This is a Temperature Converter pick from the following: ")
+           3: "Celcius To Kelvin"}
+print(f"=" * 50)
+
+print(f"This is a Temperature Converter pick from the following: ")
 print(f"=" * 50)
 
 for key, value in choices.items():
     print(f"{key} = {value}")
+           
 print(f"=" * 50)
+
 choice = int(input("Enter choice: "))
+
 print(f"=" * 50)
 
-
-if choice == 1:
-    Fahrenheit = float(input("Enter Fahrenheit: "))
-    Celsius = (Fahrenheit - 32) * 5 / 9
-
-    print("=" * 50)
-    print(f"Celsius: {Celsius:.2f}")
-
+if choice == 1: Fahrenheit = float(input("Enter Fahrenheit: ")) 
+           Celsius = (Fahrenheit - 32) * 5 / 9 print("=" * 50) print(f"Celsius: {Celsius:.2f}")
     
 elif (choice == 2):
     Celcius = int(input("Enter Celcius"))
-    Farenheit = int(input("Enter Farenheit"))
-    print(f"=" * 50)
+    Farenheit = (Celcius * 9/5) + 32
+    print(f"=" * 50) 
     
 elif (choice == 3):
-    Celcius = int(input("Enter Celcius"))
-    Kelvin = int(input("Enter Kelvin"))
+    Celcius = int(input("Enter Celcius: "))
+    celsius_kelvin = Celcius + 273.15
+    print(f"{celsius_kelvin:.2f}K")
     print(f"=" * 50)
 
 else:
     print("Invalid Choice")
-    print(f"=" * 50)Kelvin"}
-print(f"=" * 50)
-
-prin
+    print(f"=" * 50) 
